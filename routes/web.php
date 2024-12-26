b27cabadb654ffbc6eef77e6f569c8d6141b0e0f
trhmdninst <trirahmadaninst@gmail.com>
2024-12-26 23:48:55 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('managesurvey', function () {
+    return view('managesurvey');
+});
+
+Route::get('managedosen', function () {
+    return view('managedosen');
+});
