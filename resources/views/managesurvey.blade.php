4fab0d248f0a6f50c4bd72d6ebe84dde468a2327
trhmdninst <trirahmadaninst@gmail.com>
2024-12-27 14:23:44 +0700

    Halaman admin by Tri

--- resources/views/managesurvey.blade.php
+++ resources/views/managesurvey.blade.php
@@ -4,14 +4,133 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Manage Survey</title>
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <title>Manage Survey</title>  
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+  <style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+    
+      body {
+        font-family: Arial, sans-serif;
+        display: flex;
+        height: 100vh;
+        background-color: #f0f0f0;
+      }
+    
+      .sidebar {
+        width: 250px;
+        background-color: #0B7bb0;
+        color: #fff;
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+      }
+    
+      .logo-section {
+        display: flex;
+        align-items: center;
+        padding-top: 15px;
+        text-align: center;
+        gap: 10px;
+        border-bottom: 0.5px solid #DEE8F2;
+        padding-bottom: 10px;
+      }
+    
+      .logo {
+        margin-right: 10px;
+      }
+    
+      .text {
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+      }
+    
+      .text h4 {
+        font-size: 20px;
+        font-weight: bold;
+      }
+    
+      .text p {
+        font-size: 12px;
+        font-weight: normal;
+      }
+    
+    
+      .sidebar ul {
+        list-style: none;
+      }
+    
+      .sidebar ul li {
+        margin: 15px 0;
+        font-size: 16px;
+      }
+    
+      .sidebar ul li i {
+        margin-right: 10px;
+      }
+    
+      .sidebar ul li a {
+        color: #fff;
+        text-decoration: none;
+      }
+    
+      .about-section {
+        margin-top: 30px;
+      }
+    
+      .about-section h3 {
+        margin-bottom: 15px;
+        font-size: 18px;
+        padding-bottom: 10px;
+        border-bottom: 2px solid #fff;
+      }
+    
+      .social-icons {
+        margin-top: 20px;
+        display: flex;
+        gap: 10px;
+      }
+    
+      .social-icons a {
+        color: #fff;
+        font-size: 20px;
+        text-decoration: none;
+      }
+    
+      .divider {
+        width: 5px;
+        background-color: #000;
+      }
+    
+      .content {
+        flex: 1;
+        padding: 20px;
+        background-color: #e0e0e0;
+      }
+    
+      .content h1 {
+        font-size: 24px;
+        font-weight: bold;
+        padding-top: 35px;
+        padding-bottom: 10px;
+        border-bottom: 2px solid #7f7d7d;
+      }
+    
+      .content-divider {
+        margin-top: 10px;
+        border: 1px solid #000;
+      }
+  </style>
 </head>
 <body>
   <div class="sidebar">
     <div class="logo-section">
-      <img src="{{ asset('images/logo-uinsu.png') }}" alt="LOGO UINSU" class="logo-image">
+      <img src="/logo-uinsu.png" alt="Logo UINSU" width="50px" height="50px">
       <div class="text">
         <h4>Survey Dosen</h4>
         <p>Sistem Informasi Uinsu</p>
