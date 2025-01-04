6eea427941cf43bf12d96110fa08b34f6e5eb1d6
NajwaFadiyah <fadiyahn30@gmail.com>
2025-01-04 22:28:41 +0700

    Membuat model dan migration

---
+++ database/migrations/2025_01_04_150830_create_formsurveys_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('formsurvey', function (Blueprint $table) {
+            $table->id();
+            $table->integer('nim')->unique();
+            $table->string('nama_mahasiswa', 35)->nullable();
+            $table->integer('semester');
+            $table->string('nama_dosen', 35)->nullable();
+            $table->string('matakuliah', 35)->nullable();
+            // Pertanyaan
+            $table->enum('survey1', ['1', '2', '3', '4'])->nullable();
+            $table->enum('survey2', ['1', '2', '3', '4'])->nullable();
+            $table->enum('survey3', ['1', '2', '3', '4'])->nullable();
+            $table->enum('survey4', ['1', '2', '3', '4'])->nullable();
+            $table->enum('survey5', ['1', '2', '3', '4'])->nullable();
+            $table->enum('survey6', ['1', '2', '3', '4'])->nullable();
+            $table->enum('survey7', ['1', '2', '3', '4'])->nullable();
+            // akhir pertanyaan
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('formsurvey');
+    }
+};
